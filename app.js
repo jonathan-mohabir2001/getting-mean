809920996d3239d628088831351368cbfc6cf12e
Jonathan Mohabir <jonamohabir416@gmail.com>
2023-02-02 14:07:03 -0500

    Controllers updated to reflect new changes, add a review page created.

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
+/*
+Auto Generated Express code. 
+*/
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+// Route import
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
+// End of Route import
 
 const app = express();
 
+// view engine configuration
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
 
+//MIDDLEWARE
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Indicating to express application to use the created Controllers, routers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.use(function (req, res, next) {
   next(createError(404));
 });
-
-
-
+// catch a 404
 
 app.use(function (err, req, res, next) {
   res.locals.message = err.message;
@@ -35,6 +41,6 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+// render my error pug file.
 
 module.exports = app;
--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,3 +1,12 @@
+/*
+Following the MVC architecture, 
+
+This is the  contoller file. 
+
+These are ES6 arrow functions defined as their own 
+constants. 
+
+*/
 const homeList = (req, res) => {
   // new controller method will be rendering another view.
   res.render('locations-list', { title: 'Home' });
@@ -10,9 +19,11 @@ const locationInfo = (req, res) => {
 
 
 const addReview = (req, res) => {
-  res.render('index', { title: 'Add review' });
+  res.render('location-review-form', { title: 'Add review' });
+  // new pug view to be rendered. 
 };
 
+// export these arrow functions
 module.exports = {
   homeList,
   locationInfo,
