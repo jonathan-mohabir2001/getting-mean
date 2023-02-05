8cfb040aea1f673b147d004d261f05f7f1c230f9
Jonathan Mohabir <jonamohabir416@gmail.com>
2023-02-05 09:16:43 -0500

    Data transitioning from View to Controller, Home data complete.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -8,19 +8,54 @@ constants.
 
 */
 const homeList = (req, res) => {
-  // new controller method will be rendering another view.
-  res.render('locations-list', { title: 'Home' });
+  // This controller will now be updated to feed data to the home view
+
+  res.render('locations-list', {
+    // this will be an object to send to the view
+
+    title: 'LOC8R APP - Find a place with Wifi to work near you',
+    pageHeader: {
+      title: 'Loc8r',
+      strapline: 'Find places  to work with wifi near you',
+    },
+
+    // other data to be sent to view: Location info
+    // keys - Name, Address, Rating,Facitilies, Distance.
+
+    locations: [
+      {
+        name: 'Starcups',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 3,
+        facilities: ['Hot Drinks', 'Wi-Fi included', 'Food'],
+        distance: '100m',
+      },
+      {
+        name: 'OneStopCofee',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 4,
+        facilities: ['Hot Drinks', 'Wi-Fi included', 'Food'],
+        distance: '200m',
+      },
+      {
+        name: 'Burger Queen ',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 3,
+        facilities: ['Wi-Fi included', 'Food'],
+        distance: '300m',
+      },
+    ],
+  });
 };
 
 const locationInfo = (req, res) => {
   res.render('location-info', { title: 'Location info' });
 };
-// controller now renders a new view location-info. 
-
+// controller now renders a new view location-info.
 
 const addReview = (req, res) => {
   res.render('location-review-form', { title: 'Add review' });
-  // new pug view to be rendered. 
+  // new pug view to be rendered.
 };
 
 // export these arrow functions
